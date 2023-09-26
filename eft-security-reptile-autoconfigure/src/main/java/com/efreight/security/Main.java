cd2d6cdcb8c29343c3a01ed0be2eedd8bad43e4e
fuyuanhui <Fuyuanhuiabcd@123>
2023-09-26 10:45:32 +0800

    自定义starter待完善

---
+++ eft-security-reptile-autoconfigure/src/main/java/com/efreight/security/Main.java
@@ -0,0 +1,11 @@
+package com.efreight.security;
+
+/**
+ * @author fu yuan hui
+ * @date 2023-09-26 10:32:00 Tuesday
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
